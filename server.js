3825acb79e3bd426e79d990add67eb7405c411fc
Rahma Abdullahi <82878278+rahmaabdul@users.noreply.github.com>
2021-12-15 00:09:13 +0000

    server

--- server.js
+++ server.js
@@ -1,15 +1,21 @@
+//importing dependencies
 const path = require("path");
 const express = require("express");
-const fs = require("fs");
 
+//server
 const app = express();
+
+//port listener
 const PORT = process.env.PORT || 3001;
 
+//middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use("./public", express.static("./"));
 
-require("routes")(app);
+app.get("");
+
+require("routes/")(app);
 require("routes")(app);
 
 app.listen(PORT, function () {
